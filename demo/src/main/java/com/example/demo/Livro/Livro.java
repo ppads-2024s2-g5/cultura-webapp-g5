ece51d16d6757969a79441aecab20ddfbf6a33a1
Victor Souza <164567046+Vicsouza988@users.noreply.github.com>
2024-09-29 22:23:45 +0000

    criando livros

---
+++ demo/src/main/java/com/example/demo/Livro/Livro.java
@@ -0,0 +1,61 @@
+package com.example.demo.Livro;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="livros")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String autor;
+    private int numeroDePaginas;
+    private String genero;
+    private String titulo;
+
+    public Livro() {
+        super();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumeroDePaginas() {
+        return numeroDePaginas;
+    }
+
+    public void setNumeroDePaginas(int numeroDePaginas) {
+        this.numeroDePaginas = numeroDePaginas;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
