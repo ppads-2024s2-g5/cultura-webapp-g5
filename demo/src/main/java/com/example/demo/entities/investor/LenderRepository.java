e5afbad873c2637a16b04c94ee6609bb43530257
Victor Souza <164567046+Vicsouza988@users.noreply.github.com>
2024-10-31 18:50:39 +0000

    lenders

---
+++ demo/src/main/java/com/example/demo/entities/investor/LenderRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.entities.investor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LenderRepository extends JpaRepository<Lender, Long> {
+}
+
